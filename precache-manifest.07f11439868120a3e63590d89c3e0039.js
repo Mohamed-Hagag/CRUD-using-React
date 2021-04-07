97ca12d18b4b53c67642231e1c377bd465c91dc5
unknown <mo.hagag27@gmail.com>
2021-04-07 13:57:48 +0200

    Updates

---
+++ precache-manifest.07f11439868120a3e63590d89c3e0039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fd2e9593c23517923ccf958d8fc396",
+    "url": "/CRUD-using-React/index.html"
+  },
+  {
+    "revision": "7c0e9d41334702acca9d",
+    "url": "/CRUD-using-React/static/css/main.6547da6a.chunk.css"
+  },
+  {
+    "revision": "7e31ef2db4d1018a00ab",
+    "url": "/CRUD-using-React/static/js/2.de8e2a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUD-using-React/static/js/2.de8e2a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0e9d41334702acca9d",
+    "url": "/CRUD-using-React/static/js/main.a0b73541.chunk.js"
+  },
+  {
+    "revision": "cf5a995ad12d616e8d49",
+    "url": "/CRUD-using-React/static/js/runtime-main.20e388e0.js"
+  }
+]);
